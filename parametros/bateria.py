d7a706cb0548dd4accfdd4f98a55b70aa8518101
4zzuf <86991747+4zzuf@users.noreply.github.com>
2025-06-16 21:27:36 -0500

    Remove battery degradation factor

--- parametros/bateria.py
+++ parametros/bateria.py
@@ -6,20 +6,16 @@ class ParametrosBateria:
         potencia_carga=100,
         capacidad=500,
         soc_objetivo=100,
-        factor_degradacion=0.9998888,
     ):
         self.potencia_carga = potencia_carga
         self.capacidad = capacidad
         self.soc_objetivo = soc_objetivo
-        # Factor por el cual se reduce la capacidad tras cada recarga
-        self.factor_degradacion = factor_degradacion
 
     def actualizar(
         self,
         potencia=None,
         capacidad=None,
         soc_objetivo=None,
-        factor_degradacion=None,
     ):
         """Actualiza los valores de la batería según se necesite."""
         if potencia is not None:
@@ -28,5 +24,3 @@ class ParametrosBateria:
             self.capacidad = capacidad
         if soc_objetivo is not None:
             self.soc_objetivo = soc_objetivo
-        if factor_degradacion is not None:
-            self.factor_degradacion = factor_degradacion
